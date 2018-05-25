644b0f27765addfe6e18352279573fe640ad4db6
Attila Bali <v-abali@hotels.com>
2018-05-25 21:37:53 +0200

    #4 Configure TravisCI to push Docker Images to own repo

---
+++ docker/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "-------> Pushing Docker Images <--------"
+docker login "&DOCKER_REPOSITORY" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+bash docker_build.sh spring-petclinic-vets-service
---
+++ docker/docker_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ARTIFACT=$1
+
+build() {
+  cd $TRAVIS_BUILD_DIR/$ARTIFACT
+
+  MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+
+  DOCKER_IMAGE=$DOCKER_NAMESPACE/$DOCKER_REPO:$ARTIFACT
+  echo "pushing docker image: " $DOCKER_IMAGE
+  docker push $DOCKER_IMAGE
+  cd $TRAVIS_BUILD_DIR
+
+}
+
+build
