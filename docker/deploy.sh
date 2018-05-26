9d8a3e3fd678b3d556a71afb7043e47e41147af5
Attila Bali <v-abali@hotels.com>
2018-05-26 09:55:04 +0200

    #13 Fix image name in deploy script and add all images to push

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 echo "-------> Pushing Docker Images <--------"
 docker login "$DOCKER_REPOSITORY" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+bash docker/docker_build.sh spring-petclinic-tracing-server
+bash docker/docker_build.sh spring-petclinic-api-gateway
+bash docker/docker_build.sh spring-petclinic-discovery-server
+bash docker/docker_build.sh spring-petclinic-config-server
+bash docker/docker_build.sh spring-petclinic-visits-service
 bash docker/docker_build.sh spring-petclinic-vets-service
+bash docker/docker_build.sh spring-petclinic-customers-service
+bash docker/docker_build.sh spring-petclinic-admin-server
